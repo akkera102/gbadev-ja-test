a65344ff1fed6bc4300ebfc9a32f1b629366f1fc
akkera102 <akkera102@gmail.com>
2023-06-04 19:45:21 +0900

    初版

--- 116_vgm2gba/src/vgm.arm.c
+++ 116_vgm2gba/src/vgm.arm.c
@@ -48,11 +48,11 @@ cmdChk:
 
 	if(cmd == VGM_CMD_WAIT)
 	{
-		u16 tm3 = (u16)(*Vgm.pBuf++);
-		tm3    |= (u16)(*Vgm.pBuf++) << 8;
-
 		u16 tm2 = (u16)(*Vgm.pBuf++);
 		tm2    |= (u16)(*Vgm.pBuf++) << 8;
+
+		u16 tm3 = (u16)(*Vgm.pBuf++);
+		tm3    |= (u16)(*Vgm.pBuf++) << 8;
 
 		REG_TM3CNT_H = 0;
 		REG_TM2CNT_H = 0;
